f861fa136cfdd83ea92ce5f8881578f67cad7aeb
pewww <jung22337@naver.com>
2021-03-31 13:54:12 +0900

    :wrench: typescript + webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.ts'
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname + '/dist')
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js'
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+};
